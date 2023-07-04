0dc489518bf6e3f024cc698d4d0034566aa8d701
Hasan Rifat <hasanrifatr1@gmail.com>
2023-07-04 23:17:12 +0600

    all is done

---
+++ js/app.js
@@ -0,0 +1,46 @@
+import products from "./products.js";
+import { addToCart, cartFunc } from "./cart.js";
+console.log({ products });
+
+function displayProducts() {
+  const productList = document.getElementById("product-list");
+
+  for (const product of products) {
+    const { image, name, price } = product;
+    console.log({ product });
+    const singleProduct = `
+        <div class="col-sm-6 col-md-6 col-lg-6 col-xs-6">
+          <div class="shadow-sm card mb-3 product">
+            <img class="product-img" src=${image} alt="prd1" />
+            <div class="card-body">
+              <h5 class="card-title text-info bold product-name">
+                ${name}
+              </h5>
+              <p class="card-text text-success product-price">${price} $</p>
+              <button
+                class="btn badge badge-pill badge-secondary mt-2 float-right"
+                type="button"
+                id="add-to-cart"
+              >
+                Add to cart
+              </button>
+            </div>
+          </div>
+        </div>
+    `;
+    productList.insertAdjacentHTML("beforeend", singleProduct);
+  }
+
+  const addToCartButton = document.querySelectorAll("#add-to-cart");
+  addToCartButton.forEach((addToCartButton) => {
+    addToCartButton.addEventListener("click", () => {
+      const productDom = addToCartButton.parentNode.parentNode;
+      addToCart(productDom);
+    });
+  });
+}
+
+// loaded content
+document.addEventListener("DOMContentLoaded", () => {
+  displayProducts();
+});
---
+++ js/products.js
@@ -0,0 +1,7 @@
+const products = [
+  { id: 1, name: "Product 1", image: "../img/bottle.jpg ", price: 110 },
+  { id: 2, name: "Product 2", image: "../img/protector.jpg ", price: 220 },
+  { id: 3, name: "Product 3", image: "../img/usb.jpg ", price: 330 },
+];
+
+export default products;
